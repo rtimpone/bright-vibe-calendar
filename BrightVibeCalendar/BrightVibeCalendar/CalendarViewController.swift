ff217937c891d7d6e5da9ed95c9dab980bbcd36d
Rob Timpone <rob.timpone@recsolu.com>
2018-05-04 14:20:48 -0500

    Implementing basic collection view behavior

---
+++ BrightVibeCalendar/BrightVibeCalendar/CalendarViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CalendarViewController.swift
+//  BrightVibeCalendar
+//
+//  Created by Rob Timpone on 5/4/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        adjustCollectionViewSpacing()
+    }
+    
+    func adjustCollectionViewSpacing() {
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.minimumInteritemSpacing = 0
+            layout.minimumLineSpacing = 1
+        }
+    }
+}
+
+extension CalendarViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 200
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "dayCell", for: indexPath)
+    }
+}
---
+++ BrightVibeCalendar/BrightVibeCalendar/ContainerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContainerViewController.swift
+//  BrightVibeCalendar
+//
+//  Created by Rob Timpone on 5/4/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContainerViewController: UIViewController {
+    
+    
+}
